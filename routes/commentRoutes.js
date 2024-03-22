29b0066c8c4b0a345ece230bc2f8952a5b6df09f
Rishabh Parsediya <parsediyarishabh@gmail.com>
2024-03-22 18:44:13 +0530

    add auth in comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controller/CommentController");
+const auth = require("../middlewares/checkToken");
 
 router.get("/:blogid", auth, commentController.getComments);
 router.post("/", auth, commentController.create);
